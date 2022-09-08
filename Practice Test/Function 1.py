186a47203e63c2190510c843413a84eaf9dc36e8
Gruys, John <jgruys@iu.edu>
2022-09-08 12:03:43 -0400

    update?

--- Practice Test/Function 1.py
+++ Practice Test/Function 1.py
@@ -1,1 +1,9 @@
-print("") 
+import math
+a, b, c, = 1, 2, -3, 
+def d():
+    return math.sqrt(b**2 - 4 * a * c)
+
+solution1 = (-b + d) / 2 * a 
+solution2 = (-b - d) / 2 * a 
+
+print(solution1, solution2)
