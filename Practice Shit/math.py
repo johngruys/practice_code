000eafd166358b55dc4e6748571419c4ccf0caff
jgruys <jgruys@iu.edu>
2022-09-13 09:10:14 -0400

    test

--- Practice Shit/math.py
+++ Practice Shit/math.py
@@ -1,2 +1,18 @@
-import math
-print(math.sqrt(4))
+# Interface to find the exponential 
+# growth of an investment
+
+import math #duh
+
+# PERT function
+def pert(p, r, t):
+    return p * math.exp(r*t)
+
+# Have user define the principal
+print ("Please input the initial value of investment")
+p = int(input ())
+
+# Have user define the interest on the investment
+print ("Please input the rate of interest (Where '1' represents 100%)")
+r = int(input ())
+
+print (pert(p, r, t))
