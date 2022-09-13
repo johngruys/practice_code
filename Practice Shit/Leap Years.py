f1bb785681a4af24e92988372e4694c791f82bec
jgruys <jgruys@iu.edu>
2022-09-13 09:00:13 -0400

    Interface for Leap years

--- Practice Shit/Leap Years.py
+++ Practice Shit/Leap Years.py
@@ -1,10 +1,23 @@
 def f(x):
     return x%4
 
+print ("Please input a year below, then press enter.")
 x = int(input ())
 
-if f(x) == 0:
-    print ("366 days")
 
-if f(x) != 0:
-    print ("365 days")
+
+if (f(x) == 0) and (x<2022):
+    print ("The year " + str(x) + " had 366 days")
+
+if (f(x) != 0) and (x<2022):
+    print ("The year " + str(x) + " had 365 days")
+
+if (f(x) == 0) and (x>2022):
+    print ("The year " + str(x) + " will have 366 days")
+
+if (f(x) != 0) and (x>2022):
+    print ("The year " + str(x) + " will have 365 days")
+
+if x == 2022:
+    print ("This year is not a leap year.")
+    
