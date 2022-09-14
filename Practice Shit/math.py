cf6f0bf7f51910fc28c93b999ab1abe59ad1e301
jgruys <jgruys@iu.edu>
2022-09-14 11:34:22 -0400

    pert working

--- Practice Shit/math.py
+++ Practice Shit/math.py
@@ -9,10 +9,14 @@ def pert(p, r, t):
 
 # Have user define the principal
 print ("Please input the initial value of investment")
-p = int(input ())
+p = float(input ())
 
 # Have user define the interest on the investment
 print ("Please input the rate of interest (Where '1' represents 100%)")
-r = int(input ())
+r = float(input ())
+
+# Have user input the time (in years)
+print ("Please input the duration of the investment in years")
+t = float(input ())
 
 print (pert(p, r, t)) 
