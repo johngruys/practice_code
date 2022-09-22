fe65fe338e588255baf34e797e9fb507d5bd14d6
jgruys <jgruys@iu.edu>
2022-09-22 11:18:55 -0400

    lab

---
+++ Practice Stuff/Loop Practice/Lecture practice.py
@@ -0,0 +1,14 @@
+def r(x, lst):
+    newlst = []
+    for i in lst:
+        if not i == 1:
+            newlist = newlist + [i]
+    return newlist 
+
+
+
+
+x = 1
+lst = [2, 1, 3, 1, 2, 5, 1]
+print (r(x, lst))
+
