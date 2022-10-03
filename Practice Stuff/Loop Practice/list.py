25d133336561d077833aa21877d8f9fae2c2637d
jgruys <jgruys@iu.edu>
2022-10-03 12:34:36 -0400

    lecture

--- Practice Stuff/Loop Practice/list.py
+++ Practice Stuff/Loop Practice/list.py
@@ -1,2 +1,15 @@
-list = [(1, 2)]
-print(len(list))
+def lec(n, d):
+    q = 0
+    nextq = q + 1
+    while (nextq * d) < n:
+        q = nextq
+        nextq = q+1
+    
+    remainder = n - (q * d)
+    
+    return (q, remainder)
+
+print(lec(12, 2))
+
+
+    
