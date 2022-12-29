e3c22bb8fa6a3e3660147fd4ad120e6fdbe7076d
jgruys <jgruys@iu.edu>
2022-12-29 00:49:24 -0500

    si no sound

--- Games/Space Invaders/SI_Main.py
+++ Games/Space Invaders/SI_Main.py
@@ -1,5 +1,6 @@
 import pygame as py
 import random
+import math 
 
 ### Initialize Pygame ###
 py.init()
@@ -25,11 +26,28 @@ playerX_change = 0
 playerY_change = 0
 
 ### Creating Enemy ###
-enemy_image = py.image.load("Games/Space Invaders/SIAssets/alien.png")
-enemyX = random.randint(0, 734)
-enemyY = random.randint(50, 250)
-enemyX_change = 0.15
-enemyY_change = 30
+# enemy_image = py.image.load("Games/Space Invaders/SIAssets/alien.png")
+# enemyX = random.randint(0, 736)
+# enemyY = random.randint(50, 250)
+# enemyX_change = 0.15
+# enemyY_change = 30
+
+### Multiple Enemies ###
+enemy_image = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+
+num_enemies = 6
+
+for i in range(num_enemies):
+    enemy_image.append(py.image.load("Games/Space Invaders/SIAssets/alien.png"))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 250))
+    enemyX_change.append(0.15)
+    enemyY_change.append(30)
+
 
 ### Bullet ###
 laser_image = py.image.load("Games/Space Invaders/SIAssets/laser.png")
@@ -38,6 +56,17 @@ laserY = 480
 laserY_change = -.75
 laser_state = "ready"
 
+### Score ###
+score_value = 0
+font = py.font.Font("freesansbold.ttf", 32)
+
+textX = 10
+textY = 10
+
+def showscore(x, y):
+    score = font.render("Score:" + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
 
 ### fire laser function, 16 and 10 are offsets ###
 def fire_laser(x, y):
@@ -45,13 +74,21 @@ def fire_laser(x, y):
     laser_state = "fire"
     screen.blit(laser_image, (x + 8, y - 8))
 
-
-
 def player(x, y):
     screen.blit(player_image, (x, y))
 
-def enemy(x, y):
-    screen.blit(enemy_image, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemy_image[i], (x, y))
+
+def iscollision(enemyX, enemyY, laserX, laserY):
+    distance = math.sqrt(((enemyX - laserX) ** 2) + ((enemyY - laserY) ** 2))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+
 
 
 ### Game loop ###
@@ -98,16 +135,30 @@ while running:
         playerX = 0
 
     ### Moving the alien ###
-    enemyX += enemyX_change
+    # enemyX[i] += enemyX_change[i]
 
     ### Boundary ###
+    for i in range(num_enemies):
 
-    if enemyX <= 0:
-        enemyX_change = -1 * enemyX_change
-        enemyY += enemyY_change
-    if enemyX >= 734:
-        enemyX_change = -1 * enemyX_change
-        enemyY += enemyY_change
+        enemyX[i] += enemyX_change[i]
+
+        if enemyX[i] <= 0:
+            enemyX_change[i] = -1 * enemyX_change[i]
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 734:
+            enemyX_change[i] = -1 * enemyX_change[i]
+            enemyY[i] += enemyY_change[i]
+
+        collision = iscollision(enemyX[i], enemyY[i], laserX, laserY)
+        if collision:
+            laserY = 480
+            laser_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+        
 
 
     ### Bullet movement ###
@@ -119,9 +170,24 @@ while running:
         laserY = 480
         laser_state = "ready"
 
+    ### Checking for collision ### (moved to for loop)
+
+    # collision = iscollision(enemyX, enemyY, laserX, laserY)
+    # if collision:
+    #     laserY = 480
+    #     laser_state = "ready"
+    #     score += 1
+    #     print(score)
+    #     enemyX = random.randint(0, 736)
+    #     enemyY = random.randint(50, 150)
+        
+
     ### Draws the player using passed coordinates ###
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+    # enemy(enemyX, enemyY) (moved to for loop)
+
+    ### Show score ###
+    showscore(textX, textY)
 
  
     # Updates screen with all new info 
